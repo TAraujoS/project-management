083ad4e163b6cb6a716584e7af0dfee8b0bb2c8b
Thiago Scherer <tharaujo.james@gmail.com>
2024-10-22 11:54:21 -0300

    feat: server modifications

---
+++ server/ecosystem.config.js
@@ -0,0 +1,12 @@
+modeule.exports = {
+  apps: [
+    {
+      name: "project-management",
+      script: "npm",
+      args: "run dev",
+      env: {
+        NODE_ENV: "development",
+      },
+    },
+  ],
+};

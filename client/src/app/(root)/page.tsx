5e484fc5b4a464698948712128d5faee26ed4d54
Thiago Scherer <tharaujo.james@gmail.com>
2024-11-18 19:29:15 -0300

    feat: add redirect and loading screen

--- client/src/app/(root)/page.tsx
+++ client/src/app/(root)/page.tsx
@@ -1,5 +1,31 @@
+"use client";
+
+import { useGetAuthUserQuery } from "@/state/api/api";
 import HomePage from "./home/page";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return <HomePage />;
+  const { currentData, isLoading } = useGetAuthUserQuery();
+  const token = localStorage.getItem("@user:token");
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!currentData || !token) {
+      router.push("/sign-in");
+    }
+  }, [currentData, token]);
+
+  if (isLoading)
+    return (
+      <div>
+        <iframe
+          width={700}
+          height={700}
+          src="https://lottie.host/embed/f1a8b120-9ab3-4957-ab3b-f713c3c9916a/pRodU2YQJA.json"
+          className="mx-auto"
+        />
+      </div>
+    );
+  return currentData && <HomePage />;
 }

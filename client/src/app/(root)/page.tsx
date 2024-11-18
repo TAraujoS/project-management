32e68c021bc9ee1ebaf6b4253ec8eb1237164584
Thiago Scherer <tharaujo.james@gmail.com>
2024-11-18 19:57:42 -0300

    fix: loading auth screen

--- client/src/app/(root)/page.tsx
+++ client/src/app/(root)/page.tsx
@@ -6,24 +6,26 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const { currentData, isLoading } = useGetAuthUserQuery();
-  const token = localStorage.getItem("@user:token");
   const router = useRouter();
+  const token = localStorage.getItem("@user:token");
+  const { currentData, isLoading } = useGetAuthUserQuery(undefined, {
+    skip: !token,
+  });
 
   useEffect(() => {
-    if (!currentData || !token) {
+    if (!token) {
       router.push("/sign-in");
     }
-  }, [currentData, token]);
+  }, [token, router]);
 
   if (isLoading)
     return (
-      <div>
+      <div className="mx-auto mt-10">
+        <h1 className="text-center text-xl font-semibold">Carregando...</h1>
         <iframe
           width={700}
           height={700}
           src="https://lottie.host/embed/f1a8b120-9ab3-4957-ab3b-f713c3c9916a/pRodU2YQJA.json"
-          className="mx-auto"
         />
       </div>
     );
